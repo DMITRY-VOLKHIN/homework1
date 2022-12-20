4b088993eb66d7ba12486048caaa9e2404195198
Dima <vol.xin@mail.ru>
2022-12-20 22:27:22 +0300

    второй семинар 1 задача

--- List1/Program.cs
+++ List1/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 Console.WriteLine("Введите трехзначное число:");
 int N =int.Parse(Console.ReadLine());
 int b =0;
