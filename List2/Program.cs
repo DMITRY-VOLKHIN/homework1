9aa7bb64e4380bd7b615d119cd14db8139d876c3
Dima <vol.xin@mail.ru>
2022-12-21 00:03:14 +0300

    второй семинар 2 задача

---
+++ List2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Привет хозяин");
+Console.WriteLine("Введите трехзначное число:");
+int N =int.Parse(Console.ReadLine());
+int b =0;
+if (N >=100 && N < 1000) b = N  % 10;
+if (b < 3)
+    Console.WriteLine("третьей цифры нет");
+else 
+    {
+        Console.WriteLine($"Третья цифра: {b}");
+    }
