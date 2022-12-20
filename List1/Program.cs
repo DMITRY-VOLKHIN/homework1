f170a11368b7ccf083473367044c4afb52bdde00
Dima <vol.xin@mail.ru>
2022-12-20 22:26:32 +0300

    второй семинар 1 задача

---
+++ List1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трехзначное число:");
+int N =int.Parse(Console.ReadLine());
+int b =0;
+if (N >=100 && N < 1000) b = (N / 10) % 10;
+else Console.WriteLine("число кривое");
+Console.WriteLine($"Вторая цифра: {b}");
