13e1c108f17a02a0777c6cfd4b8e9bce2c02185b
Dima <vol.xin@mail.ru>
2022-12-21 20:49:47 +0300

    второй семинар третья задача

--- List3/Program.cs
+++ List3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("День недели:");
+int N = int.Parse(Console.ReadLine());
+string  s = "не день недели";
+if (N >= 1 && N <= 5)
+    {
+        s = "не выходной";
+    }
+      if (N >= 6 && N <=7)
+        {
+          s = "ВЫХОДНОЙ";
+        }
+    
+Console.Write(s);
